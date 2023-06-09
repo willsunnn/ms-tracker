3de98ae44d4c6ff7c1ab257e862e5252dc2686a1
William Sun <sunwilliam@gmail.com>
2023-06-08 17:21:34 -0700

    Moved AlertContext

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,7 @@ import './App.css';
 import NavBar from './components/NavBar';
 import * as Auth from 'firebase/auth';
 import { Persistence } from './persistence/firebase';
-import { AddAlertCallbackProvider } from './contexts/AlertContext';
 import SignInFormComponent from './components/SignInForm';
-import { useToggleTheme } from './contexts/ThemeContext';
 
 const App = () => {
 
@@ -15,23 +13,19 @@ const App = () => {
     Persistence.onUserChange(setUser)
   }, []);
 
-  const toggleTheme = useToggleTheme();
-  const onClick = () => {
-    console.log('clicked')
-    console.log(toggleTheme);
-    toggleTheme();
-  }
-
   return (
-    <AddAlertCallbackProvider>
+    <>
       <NavBar user={user}/>
-        { !user && <div className="flex items-center justify-center h-[calc(100vh-64px)]"><SignInFormComponent/></div> }
-        { user && (
-          <h1 className="text-3xl font-bold underline text-red-600">
-            {`userId=${(user!=null)? user.uid : 'null'}`}
-          </h1>
-        )}
-    </AddAlertCallbackProvider>
+      { !user && 
+        <div className="flex items-center justify-center h-[calc(100vh-64px)]">
+          <SignInFormComponent/>
+        </div> }
+      { user && (
+        <h1 className="text-3xl font-bold underline text-red-600">
+          {`userId=${(user!=null)? user.uid : 'null'}`}
+        </h1>
+      )}
+    </>
   );  
 }
 
