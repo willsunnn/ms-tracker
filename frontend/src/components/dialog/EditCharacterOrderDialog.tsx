6faea5688baf429db34aaa0e87129c99ec90095f
William Sun <sunwilliam@gmail.com>
2023-07-05 15:24:48 -0700

    Fixed issue where Draggable would stutter when dragged

--- frontend/src/components/dialog/EditCharacterOrderDialog.tsx
+++ frontend/src/components/dialog/EditCharacterOrderDialog.tsx
@@ -2,31 +2,50 @@ import React from 'react'
 import { useAuth } from '../../contexts/AuthContext'
 import { useAlertCallback } from '../../contexts/AlertContext'
 import { useApi } from '../../contexts/ApiContext'
-import { DragDropContext, Draggable, type DropResult } from 'react-beautiful-dnd'
 import { type CharacterWithMapleGgData } from 'ms-tracker-library'
-import { StrictModeDroppable } from '../helper/StrictModeDroppable'
 import { CharacterView } from '../CharacterView'
+import { DndContext, type DragEndEvent, useDroppable } from '@dnd-kit/core'
+import { CSS as DndCss } from '@dnd-kit/utilities'
+import { SortableContext, arrayMove, useSortable } from '@dnd-kit/sortable'
 
-const CharacterCard = (props: { character: CharacterWithMapleGgData }) => {
+const CharacterDraggableCard = (props: { character: CharacterWithMapleGgData }) => {
   const { character } = props
   const name = character.mapleGgData?.name ?? character.name
   const className = character.mapleGgData?.class ?? ''
   const level = character.mapleGgData?.level ? `Lvl ${character.mapleGgData.level}` : ''
 
-  return (<div className='w-full card card-body bg-base-200 shadow-sm gap-y-1 p-2 items-center'>
-    <div className='w-2/3 grid grid-cols-2'>
+  const { attributes, listeners, setNodeRef, transform, transition } = useSortable({
+    id: character.id
+  })
+  const style = {
+    transform: DndCss.Translate.toString(transform),
+    transition
+  }
 
-    <div className='h-24 w-24 row-span-5'>
-      <CharacterView character={character} showName={false}/>
+  return (
+    <div ref={setNodeRef} style={style} {...attributes} {...listeners}>
+      <div className='h-28 w-96 card card-body bg-base-200 shadow-sm gap-y-1 p-2 grid grid-cols-2'>
+        <div className='h-24 w-48 row-span-5'>
+          <CharacterView character={character} showName={false}/>
+        </div>
+        <div/>
+        <div className='text-lg font-bold -mb-2 truncate mr-4'>{name}</div>
+        <div className='text-sm mr-4'>{className}</div>
+        <div className='text-sm -mt-3 mr-4'>{level}</div>
+        <div/>
+      </div>
     </div>
-    <div/>
-    <div className='text-lg font-bold -mb-2 truncate mr-4'>{name}</div>
-    <div className='text-sm mr-4'>{className}</div>
-    <div className='text-sm -mt-3 mr-4'>{level}</div>
-    <div/>
+  )
+}
 
+const CharacterDroppable = (props: { children?: React.ReactNode }) => {
+  const { isOver, setNodeRef } = useDroppable({ id: 'character-list-droppable' })
+  return (
+    <div ref={setNodeRef} className="flex flex-col space-y-3 items-center px-4 pt-2 pb-4">
+      <div className='w-full text-center font-semibold'>Drag and drop characters to reorder them</div>
+      {props.children}
     </div>
-  </div>)
+  )
 }
 
 export const EditCharacterOrderComponent = (props: { characters: CharacterWithMapleGgData[] }) => {
@@ -36,24 +55,19 @@ export const EditCharacterOrderComponent = (props: { characters: CharacterWithMa
 
   const [characters, setCharacters] = React.useState<CharacterWithMapleGgData[]>(props.characters)
 
-  const onDragEnd = (result: DropResult) => {
-    if (!result.destination) {
-      return
-    }
-    if (result.destination.index === result.source.index) {
-      return
-    }
-    const charToMove = characters[result.source.index]
-    const remainingChars = characters.slice(0, result.source.index).concat(characters.slice(result.source.index + 1))
-    const finalOrder = [...remainingChars.slice(0, result.destination.index), charToMove, ...remainingChars.slice(result.destination.index)]
+  // If the parent gets an update here, we need to update this component
+  React.useEffect(() => {
+    setCharacters(props.characters)
+  }, [props.characters])
 
-    // Update the local model
-    setCharacters(finalOrder)
+  // Helper function to update local state and send state to Database
+  const setNewOrder = (val: CharacterWithMapleGgData[]) => {
+    setCharacters(val)
 
     // Send update to firebase
     if (user) {
       characterApi.set(user, {
-        characters: finalOrder
+        characters: val
       }).then(() => {
         alert({
           text: 'Successfully updated character oder',
@@ -63,29 +77,26 @@ export const EditCharacterOrderComponent = (props: { characters: CharacterWithMa
     }
   }
 
+  // Event Handler
+  const onDragEnd = (event: DragEndEvent) => {
+    const { active, over } = event
+    if (over && active.id !== over?.id) {
+      const activeIndex = characters.findIndex((character) => character.id === active.id)
+      const overIndex = characters.findIndex((character) => character.id === over.id)
+      const newArray = arrayMove(characters, activeIndex, overIndex)
+      setNewOrder(newArray)
+    }
+  }
+
   return (
-  <DragDropContext onDragEnd={onDragEnd}>
-    <StrictModeDroppable droppableId={'character-list'}>
-      {provided => (
-        <div ref={provided.innerRef} {...provided.droppableProps} className="grid grid-cols-1 gap-8 px-4 pt-2 pb-4">
-            <div className='w-full text-center font-semibold'>Drag and drop characters to reorder them</div>
-            {characters.map((character: CharacterWithMapleGgData, index: number) => {
-            // For each character we return a card that is draggable
-            // that contains the TaskViewSingleCharacter
-              return (
-                <Draggable draggableId={character.id} index={index} key={`TaskViewSingleCharacter-${character.name}`}>
-                  {provided => (
-                    <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps} className="w-full">
-                      <CharacterCard character={character}/>
-                    </div>
-                  )}
-                </Draggable>
-              )
-            })}
-          { provided.placeholder }
-        </div>
-      )}
-    </StrictModeDroppable>
-  </DragDropContext>
+    <DndContext onDragEnd={onDragEnd}>
+      <SortableContext items={characters}>
+        <CharacterDroppable>
+          {characters.map((character) =>
+            (<CharacterDraggableCard key={`draggable-card-${character.name}`} character={character}/>))
+          }
+        </CharacterDroppable>
+      </SortableContext>
+    </DndContext>
   )
 }
