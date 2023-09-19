3aba4bf74fc3843afaf2ad998d45508f6a7bd03e
William Sun <sunwilliam@gmail.com>
2023-09-19 16:15:17 -0700

    Fixed issue where Animation wouldnt trigger on refresh

--- frontend/src/components/helper/AnimateOnceInViewport.tsx
+++ frontend/src/components/helper/AnimateOnceInViewport.tsx
@@ -3,31 +3,34 @@ import 'animate.css/animate.min.css'
 
 export const AnimateOnceInViewport = (props: { children: React.ReactNode, animateIn: string, className?: string }) => {
   const { className, children, animateIn } = props
-  const [triggered, setTriggered] = React.useState<boolean>(false)
-  const ref = React.createRef<HTMLDivElement>()
+  const [intersected, setIntersected] = React.useState<boolean>(false)
+
+  const [element, setElement] = React.useState<HTMLDivElement | null>(null)
+  const observerRef = React.useRef<IntersectionObserver | null>(null)
 
   React.useEffect(() => {
-    const observer = new IntersectionObserver((entries, observer) => {
-      if (!(ref.current)) {
-        return
-      }
-      if (entries[0].intersectionRatio < 0.3) {
-        return
-      }
-      setTriggered(true)
-      observer.disconnect()
-    }, { root: null, threshold: 0.5 })
-
-    if (ref.current) {
-      observer.observe(ref.current)
+    if (!element) {
+      return
     }
+    if (observerRef.current) {
+      observerRef.current.disconnect()
+    }
+    const observer = new IntersectionObserver((entries, observer) => {
+      if (intersected || !(element) || entries[0].intersectionRatio < 0.1) {
+        return
+      }
+      setIntersected(true)
+    }, { root: null, threshold: 0.1, rootMargin: '-50px' })
+
+    observer.observe(element)
+    observerRef.current = observer
 
     return () => {
       observer.disconnect()
     }
-  }, [])
+  }, [element])
   return (
-    <div ref={ref} className={`${(triggered ? `animate__animated animate__${animateIn}` : 'invisible')} ${className ?? ''}`}>
+    <div ref={setElement} className={`${(intersected ? `animate__animated animate__${animateIn}` : 'invisible')} ${className ?? ''}`}>
       {children}
     </div>
   )
